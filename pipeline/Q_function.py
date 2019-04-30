30fcff1839a08cf742a30c9b9264225be9d1c744
Ronan Perry <rflperry@gmail.com>
2019-04-30 01:18:33 -0400

    load poolings

--- pipeline/Q_function.py
+++ pipeline/Q_function.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 def Q_func(x, adj, w, embed, p, T, initialization_stddev=None,
-           scope='', reuse=False, train=True, theta_list=None, pre_pooling_mlp_layers = 1, post_pooling_mlp_layers = 1):
+           scope='', reuse=False, train=True, sess=None, pre_pooling_mlp_layers = 1, post_pooling_mlp_layers = 1):
     """
     x:      B x n_vertices.
     Placeholder for the current state of the solution.
@@ -41,6 +41,14 @@ def Q_func(x, adj, w, embed, p, T, initialization_stddev=None,
                     bs_post_pooling.append(tf.Variable(tf.random_normal([p], stddev=initialization_stddev),
                                             name='b_MLP_post_pooling_' + str(i)))
         else:
+            # load theta's and layers from the saved session
+            prefix = "q_func"
+            theta_list = [tf.convert_to_tensor(sess.run(prefix + "/thetas/theta" + str(i) + ":0"),
+                            name='theta%d' % i) for i in range(1,8)]
+            Ws_pre_pooling = [sess.run(prefix + "/pre_pooling_MLP/W_MLP_pre_pooling_%d:0") for i in range(pre_pooling_mlp_layers)]
+            bs_pre_pooling = [sess.run(prefix + "/pre_pooling_MLP/b_MLP_pre_pooling_%d:0") for i in range(pre_pooling_mlp_layers)]
+            Ws_post_pooling = [sess.run(prefix + "/pre_pooling_MLP/W_MLP_post_pooling_%d:0") for i in range(post_pooling_mlp_layers)]
+            bs_post_pooling = [sess.run(prefix + "/pre_pooling_MLP/b_MLP_post_pooling_%d:0") for i in range(post_pooling_mlp_layers)]
             # Unpack passed theta's recovered from training session.
             theta1,theta2,theta3,theta4,theta5,theta6,theta7 = theta_list
 
