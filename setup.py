59fcb04421654f2d580cbf8a12382dc776edc0d3
Ronan Perry <rflperry@gmail.com>
2019-04-30 15:27:17 -0400

    setup file with some deps

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='dl-scsp',
+      version='0.1',
+      description='DLDO Final Project',
+      url='http://github.com/rflperry/dl-scsp/',
+      author='Ronan Perry, Hadley VanRenterghem, Vinay Ayyappan, Joanna Guo',
+      author_email='rperry27@jhu.edu',
+      install_requires=[
+          'networkx',
+          'numpy',
+          
+      ],
+      zip_safe=False)
