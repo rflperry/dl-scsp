f748469e0cde18a59c3cefaafeee204e7a49611d
Ronan Perry <rflperry@gmail.com>
2019-04-30 00:37:53 -0400

    fix

--- pipeline/dqn_graph_model_v2.py
+++ pipeline/dqn_graph_model_v2.py
@@ -360,7 +360,7 @@ def test(sess, env, modelfile): # writen to look at a single test graph at a tim
     saver.restore(sess,tf.train.latest_checkpoint('/tmp/saved_models/'))
 
     # load theta's from the saved session
-    theta_list = [sess.run("q_func/thetas/theta" + i + ":0") for i in range(1,8)]
+    theta_list = [sess.run("q_func/thetas/theta" + str(i) + ":0") for i in range(1,8)]
 
     exp_name = env.env_name
     logz.configure_output_dir('data/' + exp_name + time.strftime('%m-%d-%Y-%H:%M:%s'))
